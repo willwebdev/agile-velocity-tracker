c54f214760a946033b38804cc9e6f4aea549a7ae
Will Barden <5765667+willwebdev@users.noreply.github.com>
2018-10-21 20:42:52 +0100

    Testing environment variables

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 use App\Helpers\Velocity;
 
 $router->get('/', function () use ($router) {
+    die(env("APP_ENV"));
+
     return view('main', [
     	'title' => "Agile velocity tracker",
     	'header' => "Agile velocity tracker",
