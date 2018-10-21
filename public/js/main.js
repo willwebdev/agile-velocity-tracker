714e9ae8c3e7e85683398d5517ae58546126916c
Will Barden <5765667+willwebdev@users.noreply.github.com>
2018-10-21 21:10:27 +0100

    Bit of styling and velocity report

--- public/js/main.js
+++ public/js/main.js
@@ -10,6 +10,12 @@ var vmHome = new Vue({
 		calculateVelocity: function() {
 			var allScores = this.$refs.sprint_scores.value.split(",");
 			this.$http.post('/calculate-velocity', {scores: allScores}).then(response => {
+				this.content = "<p class=\"velocity-report\">"
+				+ "<b>Velocity avg</b>: <span class=\"avg\">" + Math.round(response.body.average) + "</span> (variance range of "
+				+ "<span class=\"lower\">" + Math.round(response.body.average - response.body.variance) + "</span> to "
+				+ "<span class=\"upper\">" + Math.round(response.body.average + response.body.variance) + "</span>)"
+				+ "</p>";
+
 				var latestScores = allScores.slice(-6);
 				var target = document.getElementById('chart');
 				var xVals = [1,2,3,4,5,6,7,8,9,10,11,12];
--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -1,7 +1,7 @@
 
 <div id="calculate-velocity">
 	<form v-on:submit.prevent="calculateVelocity">
-		<p><span>Enter your sprint scores (comma separated):</span> <input type="text" size="50" ref="sprint_scores" /> <button v-on:click="calculateVelocity" id="calculate-velocity">Calculate velocity</button></p>
+		<p class="intro"><span>Enter your sprint scores (comma separated):</span> <input type="text" size="50" ref="sprint_scores" /> <button v-on:click="calculateVelocity" id="calculate-velocity">Calculate velocity</button></p>
 		<div v-html="content">Please enable Javascript...</div>
 		<div id="chart"></div>
 	</form>
